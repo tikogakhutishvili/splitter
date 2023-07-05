4054359a972f2203ae4e4dd285ee3e0809d00d20
tikogakhutishvili <tikogakhutishvili26@gmail.com>
2023-07-05 22:54:05 +0400

    splitter

--- main.js
+++ main.js
@@ -10,77 +10,51 @@ const oneperson = document.getElementById("oneperson")
 const total = document.getElementById("total")
 const reset = document.getElementById("reset")
 const required = document.querySelector("#required")
-const money = []
-const num = []
-const percent = []
-
-fiveperc.addEventListener('click', () => {
-    money.push(typefee.value)
-    num.push(peoplenum.value)
-    five()
-})
-
-function five() {
-
-    oneperson.innerHTML = money.map(el => `<h2>$${el*0.05}</h2>`),
-    total.innerHTML = money.map(el => `<h2>$${el*0.05*num}</h2>`)
-    
-}
+const inps = document.querySelectorAll("input")
+const calc = document.querySelector("#calc")
 
 
-tenperc.addEventListener('click', () => {
-    money.push(typefee.value)
-    num.push(peoplenum.value)
-    ten()
-})
-function ten() {
-    oneperson.innerHTML = money.map(el => `<h2>$${el*0.10}</h2>`)
-    total.innerHTML = money.map(el => `<h2>$${el*0.10*num}</h2>`)
-}
-
-fifteenperc.addEventListener('click', () => {
-    money.push(typefee.value)
-    num.push(peoplenum.value)
-    fifteen()
-})
-function fifteen() {
-    oneperson.innerHTML = money.map(el => `<h2>$${el*0.15}</h2>`)
-    total.innerHTML = money.map(el => `<h2>$${el*0.15*num}</h2>`)
-}
-
-
-twentyfiveperc.addEventListener('click', () => {
-    money.push(typefee.value)
-    num.push(peoplenum.value)
-    twentyfive()
-})
-function twentyfivefive() {
-    oneperson.innerHTML = money.map(el => `<h2>$${el*0.25}</h2>`)
-    total.innerHTML = money.map(el => `<h2>$${el*0.25*num}</h2>`)
-}
-fiftyperc.addEventListener('click', () => {
-    money.push(typefee.value)
-    num.push(peoplenum.value)
-    fifty()
-})
-function fifty() {
-    oneperson.innerHTML = money.map(el => `<h2>$${el*0.5}</h2>`)
-    total.innerHTML = money.map(el => `<h2>$${el*0.5*num}</h2>`)
-}
-addEventListener('keypress', (e) => {
-    if(e.key === 'Enter') {
-
-        money.push(typefee.value)
-        num.push(peoplenum.value)
-        percent.push(typeperc.value/100)
-        Result()
+calc.addEventListener("click", () => {
+    if(peoplenum.value === "") {
+        peoplenum.style.borderColor= "#E17052;"
+        required.classList.replace("hide","show")
+    } else {
+        return result (typeperc.value),
+        typefee.style.border = "none",
+        required.classList.replace("show","hide")
     }
 })
-function Result() {
-    oneperson.innerHTML = money.map(el => `<h2>$${el*percent*num}</h2>`)
-    total.innerHTML = money.map(el => `<h2>$${el*percent*num}</h2>`)
+
+
+function result(e) {
+    let tip = (((typefee.value*e)/100) / peoplenum.value)
+    oneperson.innerHTML = "$" + Number.parseFloat(tip).toFixed(2),
+    total.innerHTML = "$" + Number.parseFloat(typefee.value / peoplenum.value + tip).toFixed(2)   
 }
+
+fiveperc.addEventListener('click', () => {
+    typeperc.value = 5
+})
+tenperc.addEventListener('click', () => {
+    typeperc.value = 10
+})
+fifteenperc.addEventListener('click', () => {
+    typeperc.value = 15
+})
+twentyfiveperc.addEventListener('click', () => {
+    typeperc.value = 25
+})
+fiftyperc.addEventListener('click', () => {
+    typeperc.value = 50
+})
+
+
+
 reset.addEventListener("click", () =>{
-    location.reload()
+    oneperson.innerHTML = "$0.00"
+    total.innerHTML = "$0.00"
+    typefee.value = ""
+    typeperc.value= ""
+    peoplenum.value= ""
 } )
 
